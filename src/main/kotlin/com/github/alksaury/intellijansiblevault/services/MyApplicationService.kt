152c6da38d60079318853c8e1f7dcbf378127d58
GitHub Action <action@github.com>
2022-01-18 13:32:32 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/alksaury/intellijansiblevault/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.alksaury.intellijansiblevault.services
+
+import com.github.alksaury.intellijansiblevault.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
